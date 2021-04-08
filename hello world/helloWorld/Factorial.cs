a9fb3c40fc3b3da20fd57dfc86b80d65cf32253c
Rutvik  Savsani <savsanirutviklivetolearn@gmail.com>
2021-04-08 12:25:54 +0530

    part of assignment 2 c#

---
+++ hello world/helloWorld/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hello_world.helloWorld
+{
+    class Factorial
+    {
+
+        public int fact(int a)
+        {
+            if(a==1){
+                return 1;
+            }
+            else
+            {
+                return fact(a - 1) * a;
+            }
+        }
+    }
+}
